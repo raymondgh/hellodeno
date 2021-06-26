9fac6a3205d79735730be64487751357b0320554
Ray Harris <raymondgh@gmail.com>
2021-06-26 12:37:11 -0700

    edit text

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello Wurld!", {
+  const response = new Response("Ray was here", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
