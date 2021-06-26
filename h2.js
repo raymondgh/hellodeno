9014200c86e91f609177978839c26fef697c7fcd
Ray Harris <raymondgh@gmail.com>
2021-06-26 12:43:48 -0700

    test auto deploy

--- h2.js
+++ h2.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Ray was here", {
+  const response = new Response("No, this is Patrick.", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
